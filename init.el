4c16371bf69090b8e8e4729da48c9470db7a0739
Zachary Romero <zacromero@posteo.net>
2021-12-01 16:04:16 -0700

    Use avy timer instead

--- init.el
+++ init.el
@@ -648,7 +648,7 @@
 ;; C-c TAB crux-indent-rigidly-and-copy-to-clipboard
 ;; C-c . ... apply-operation-to-number-at-point
 
-(global-set-key (kbd "C-'") #'avy-goto-char-2)
+(global-set-key (kbd "C-'") #'avy-goto-char-timer)
 (global-set-key (kbd "M-g f") #'avy-goto-line)
 (global-set-key (kbd "M-g w") #'avy-goto-word-1)
 
